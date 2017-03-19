14d4b4ca82d6a57932a2441b89ec5ebf40d42e04
vistipuhal <vishaltipu@gmail.com>
2017-03-19 23:15:20 +0530

    Add match script

--- app/dashboard/index.js
+++ app/dashboard/index.js
@@ -10,11 +10,17 @@
     	amount_received : amount,
     	bonus : referral_bonus,
     	activate : function() {
+			$.post(settings.BASE_URL + 'back-end/match.php', function(data, status) {
+             	console.log(data);
+             	if(status == 'success') {	
+					console.log(data); 
+             	}
+            },'json');
+
             $.post(settings.BASE_URL + 'back-end/dashboard.php', function(data, status) {
              	console.log(data);
              	console.log(status);
-             	if(data.status == "true")
-             	{	
+             	if(data.status == "true") {	
 					payment_received(data.pay_received);
 					payment_made(data.pay_made);
 					amount(data.amount_recv);
@@ -24,7 +30,8 @@
              		router.navigate('login');
              	if(data.route_to_package == "true")
              		router.navigate('plans');
-            },'json');    	
+            },'json');
+
         },
 		attached : function() {
 	       $('#timer').flipcountdown({size:"lg"});
