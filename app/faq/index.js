c470aa172b31c7a8596eef0363e991505f34c7a4
vistipuhal <vishaltipu@gmail.com>
2017-04-01 13:00:18 +0530

    Added the accordian class in faq page

--- app/faq/index.js
+++ app/faq/index.js
@@ -1,15 +1,30 @@
 define(['durandal/app', 'knockout','durandal/system', 'plugins/router', 'settings'], function (app, ko, system, router, settings) {
 
 	return {
-		func:function()
+		drop:function()
 		{
-			$(document).ready(function(){
-				$('#f1').text('1');
-				$('#f1').attr("visibility", "visible");
+			
+			var that = event.target;
+			that.classList.toggle("active");
+			//that.className.style = "border-bottom-style: 'groove'";
+			
+			var panel = that.nextElementSibling;
+			if(panel.style.display == 'block'){
+				panel.style.display = "none";
+			}
+			else{
+				panel.style.display = "block";
+			}
 
-			})
+
+			if (panel.style.maxHeight){
+      			panel.style.maxHeight = null;
+    		} 
+    		else {
+      		panel.style.maxHeight = panel.scrollHeight + "px";
+    		} 
+
 		}	
-
 
 	};
 });
