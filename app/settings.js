af6d5684b58a460f50946edde49b848b8fda0c18
vistipuhal <vishaltipu@gmail.com>
2017-04-02 20:11:14 +0530

    ;

--- app/settings.js
+++ app/settings.js
@@ -9,6 +9,7 @@ define(['knockout'], function(ko) {
                 { route: 'signup',	         moduleId: 'signup/index',       title: 'Join Now',   nav: false },
                 { route: 'login',  			 moduleId: 'login/index',        title: 'Sign In',    nav: false },
                 { route: 'dashboard',   	 moduleId: 'dashboard/index',    title: 'Dashboard',  nav: false },
+                { route: 'transactions',     moduleId: 'transactions/index', title: 'Transactions', nav: false },
                 { route: 'plans',            moduleId: 'plans/index',        title: 'Plans',      nav: false},
                 { route: 'about',            moduleId: 'about/index',        title: 'About Us',   nav: false },
                 { route: 'faq',              moduleId: 'faq/index',          title: 'FAQ',        nav: false },
