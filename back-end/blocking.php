112757f56313f604050d1b413d5bc2a21d74fb47
vistipuhal <vishaltipu@gmail.com>
2017-04-22 19:48:01 +0530

    Blocking.php

--- back-end/blocking.php
+++ back-end/blocking.php
@@ -5,7 +5,7 @@
 	$TIME_INTERVAL = 'PT1H';
 
 
-	$sql_timer = "SELECT a.time_stamp AS `time_stamp`, u.user_id AS `user_id` FROM `transaction_details` a JOIN `user` u ON a.user_id_receiver = u.user_id AND a.have_paid = '0' AND a.user_id_receiver IS NOT NULL";
+	$sql_timer = "SELECT a.time_stamp AS `time_stamp`, u.user_id AS `user_id` FROM `transaction_details` a JOIN `user` u ON a.user_id_donor = u.user_id AND a.have_paid = '0' AND a.user_id_receiver IS NOT NULL";
 
 
 	$now = date_create();
@@ -27,11 +27,13 @@
 					$sql_block = "UPDATE `user` SET `user_blocked` = '1' WHERE `user_id` = '{$row['user_id']}'"; 
 					$res_block = mysqli_query($connection, $sql_block);
 					
-					$sql_block2 = "UPDATE `transaction_details` SET `user_id_receiver`= NULL WHERE `user_id_donor`='{$row['user_id']}' AND `have_paid`='0'";
+					$sql_block2 = "DELETE FROM `transaction_details` WHERE `user_id_donor`='{$row['user_id']}' AND `have_paid`='0'";
 			        $res_block2=mysqli_query($connection,$sql_block2);
 
 			        $sql_block3 = "UPDATE `transaction_details` SET `user_id_receiver`=NULL WHERE `user_id_receiver`='{$row['user_id']}' AND `have_paid`='0'";
 			        $res_block3 = mysqli_query($connection,$sql_block3);
+
+			        echo $sql_block3;
 
 					
 				
